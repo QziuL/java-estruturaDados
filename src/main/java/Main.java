a5a9f84874b7dd010faa2e72ba63be1cd92d3f17
QziuL <lf.quinholi2@gmail.com>
2024-06-14 12:22:04 -0300

    feat: stack-pilha

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
 import br.qziul.estruturaDeDados.LinkedList;
+import br.qziul.estruturaDeDados.Stack;
 
 public class Main {
     public static void main(String[] args) {
+        Stack stack = new Stack("Elemento 1");
+        stack.getTop();
+        stack.getHeight();
+        stack.print();
+
+/*
+        Métodos para LinkedList
         LinkedList lista = new LinkedList("Elemento 1");
         lista.append("Elemento 2");
         lista.append("Elemento 3");
@@ -13,11 +21,12 @@ public class Main {
 
         lista.print();
 
-//        lista.insert(3, "Elemento 2,5");
-//        lista.set(1, "MUDEI O COISO");
-//        lista.remove(1);
-//        lista.makeEmpty();
-//        System.out.println(lista.get(2).data);
-//        System.out.println("\nItem deletado: "+lista.removeFirst().data);;
+        lista.insert(3, "Elemento 2,5");
+        lista.set(1, "MUDEI O COISO");
+        lista.remove(1);
+        lista.makeEmpty();
+        System.out.println(lista.get(2).data);
+        System.out.println("\nItem deletado: "+lista.removeFirst().data);;
+*/
     }
 }
---
+++ src/main/java/br/qziul/estruturaDeDados/Stack.java
@@ -0,0 +1,61 @@
+package br.qziul.estruturaDeDados;
+
+/*
+    Operações da Pilha:
+    - Adicionar (push)
+    - Remover (pop)
+    - Ler topo (getTop)
+    - Imprimir (print)
+ */
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class Stack {
+    private Node top;
+    private int height;
+
+
+//  Classe que representa um Nó na pilha.
+    class Node {
+        private String data;
+        private Node next;
+
+        public Node(String data) {
+            this.data = data;
+        }
+    }
+
+//  Construtor.
+    public Stack(String data) {
+        Node newNode = new Node(data);
+        this.top = newNode;
+        this.height = 1;
+    }
+
+    public void getTop() {
+        System.out.println((this.top == null)
+                ? "Pilha vazia."
+                : "Topo: " + this.top.data
+        );
+    }
+
+    public void getHeight() {
+        System.out.println((this.height == 0)
+                ? "Pilha vazia."
+                : "Altura: "+this.height
+        );
+    }
+
+    public void print() {
+        Node varTemp = top;
+
+        System.out.println("====================");
+        while(varTemp != null) {
+            System.out.println(varTemp.data);
+            varTemp = varTemp.next;
+        }
+        System.out.println("====================");
+    }
+}
