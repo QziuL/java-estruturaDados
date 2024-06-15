843db4a4ddbea69246731b50c9884698ddebacc2
QziuL <lf.quinholi2@gmail.com>
2024-06-15 01:46:27 -0300

    feat: inverted stack

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import br.qziul.estruturaDeDados.Stack;
 
 public class Main {
     public static void main(String[] args) {
+/*
+        == Métodos para Stack (Pilha) ==
         Stack stack = new Stack("Elemento 1");
         stack.push("Elemento 2");
         stack.push("Elemento 3");
@@ -10,14 +12,21 @@ public class Main {
         stack.getTop();
         stack.getHeight();
         stack.print();
-
         System.out.println("Elemento deletado: "+stack.pop().getData());
-
-
         stack.print();
+*/
+
+        // inverter pilha
+        String[] elementos = {"Elemento 1", "Elemento 2", "Elemento 3", "Elemento 4"};
+        for (String elemento : elementos) {
+            System.out.println(elemento);
+        }
+        System.out.println("====================");
+        inverterPilha(elementos);
+
 
 /*
-        Métodos para LinkedList
+        == Métodos para Linked List (Lista Encadeada) ==
         LinkedList lista = new LinkedList("Elemento 1");
         lista.append("Elemento 2");
         lista.append("Elemento 3");
@@ -36,5 +45,18 @@ public class Main {
         System.out.println(lista.get(2).data);
         System.out.println("\nItem deletado: "+lista.removeFirst().data);;
 */
+    }
+
+    private static void inverterPilha(String[] elementos) {
+        Stack stack = new Stack(elementos[0]);
+        for(int i = 1; i < elementos.length; i++){
+            stack.push(elementos[i]);
+        }
+
+        Stack.Node elementoDeletado = stack.pop();
+        while(elementoDeletado != null) {
+            System.out.println(elementoDeletado.getData());
+            elementoDeletado = stack.pop();
+        }
     }
 }
